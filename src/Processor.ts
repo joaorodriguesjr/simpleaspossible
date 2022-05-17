f5f3f5ef6dd082734eda25d57a63705e5c0e46fe
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-05-17 11:10:52 -0300

    Add the memory read and write methods

--- src/Processor.ts
+++ src/Processor.ts
@@ -3,4 +3,41 @@ import { byte, nibble } from './helpers'
 /**
  * The SAP-1 central processing unit.
  */
-export class Processor {}
+export class Processor {
+
+  /**
+   * The cpu memory.
+   */
+  private memory: number[]
+
+  /**
+   * Initializes the cpu.
+   * - Fills the 16-byte memory with zeros.
+   */
+  public constructor() {
+    this.memory = new Array(16).fill(0)
+  }
+
+  /**
+   * Reads one of the 16 bytes of the memory.
+   * - The provided address value will be clamped by 4-bits.
+   *
+   * @param address 4-bit wide address
+   * @returns 8-bit wide value
+   */
+  public read(address: number): number {
+    return byte(this.memory[nibble(address)])
+  }
+
+  /**
+   * Writes one of the 16 bytes of the memory.
+   * - The provided address value will be clamped by 4-bits.
+   * - The provided value will be clamped by 8-bits.
+   *
+   * @param address 4-bit wide address
+   * @param value 8-bit wide value
+   */
+  public write(address: number, value: number): void {
+    this.memory[nibble(address)] = byte(value)
+  }
+}
--- test/Processor.test.ts
+++ test/Processor.test.ts
@@ -2,3 +2,12 @@ import assert from 'node:assert/strict'
 import { Processor } from '../src/Processor'
 
 const processor = new Processor()
+
+/**
+ * Should write and read a clamped 8-bit value from the memory.
+ */
+processor.write(0x0001, 0x000A)
+assert.equal(processor.read(0x0001), 0x000A)
+
+processor.write(0x0002, 0x0100)
+assert.equal(processor.read(0x0002), 0x0000)
