2645e180cb77bcf47878d02a334c7db3f43e14cd
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-05-17 12:47:23 -0300

    Remove empty space

--- test/Processor.test.ts
+++ test/Processor.test.ts
@@ -17,21 +17,21 @@ assert.equal(processor.read(0x0002), 0x0000)
  * The program counter register should be a
  * clamped 4-bit wide value.
  */
- processor.pc = 0x000A
- assert.equal(processor.pc, 0x000A)
+processor.pc = 0x000A
+assert.equal(processor.pc, 0x000A)
 
- processor.pc = 0x0010
- assert.equal(processor.pc, 0x0000)
+processor.pc = 0x0010
+assert.equal(processor.pc, 0x0000)
 
 
- /**
-  * The program counter shoud be incremented by 1
-  * after each sucessful cycle.
-  */
- processor.pc  = 0x0000
+/**
+ * The program counter shoud be incremented by 1
+ * after each sucessful cycle.
+ */
+processor.pc  = 0x0000
 
- processor.cycle()
- assert.equal(processor.pc, 0x0001)
+processor.cycle()
+assert.equal(processor.pc, 0x0001)
 
- processor.cycle()
- assert.equal(processor.pc, 0x0002)
+processor.cycle()
+assert.equal(processor.pc, 0x0002)
